e9169ab55a78ca85f272149e53f044011f8160a0
StarvingArtists <101067392+StarvingArtists@users.noreply.github.com>
2022-05-11 16:13:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,11 @@ const cookie = process.env.COOKIE
 const groupId = process.env.GROUP
 
 app.get("/users/:userId", async (req, res) => {
-    fetch(`https://firestore.googleapis.com/v1beta1/projects/starving-artists/databases/(default)/documents/${req.params.userId}`, {
-        
-    })
+    fetch(`https://firestore.googleapis.com/v1beta1/projects/starving-artists/databases/(default)/documents/users/${req.params.userId}`)
         .then(result => result.json())
         .then(json => {
-            console.log(json)
-
             return res.json({
-                pending: json
+                art: json
             })
         });
 })
